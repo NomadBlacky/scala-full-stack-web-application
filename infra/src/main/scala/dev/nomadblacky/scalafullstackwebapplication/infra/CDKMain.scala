2b7dc24681844501524d16b5e5b75031b5d98acc
NomadBlacky <tkadowaki.dev.blackey@gmail.com>
2019-12-27 19:32:54 +0900

    ECS FargateでPlayを動かす

--- infra/src/main/scala/dev/nomadblacky/scalafullstackwebapplication/infra/CDKMain.scala
+++ infra/src/main/scala/dev/nomadblacky/scalafullstackwebapplication/infra/CDKMain.scala
@@ -2,7 +2,13 @@ package dev.nomadblacky.scalafullstackwebapplication.infra
 
 import software.amazon.awscdk.core.{App, Construct, Stack}
 import software.amazon.awscdk.services.ec2.{Vpc, VpcProps}
-import software.amazon.awscdk.services.ecs.{Cluster, ClusterProps}
+import software.amazon.awscdk.services.ecs._
+import software.amazon.awscdk.services.ecs.patterns.{
+  ApplicationLoadBalancedFargateService,
+  ApplicationLoadBalancedFargateServiceProps
+}
+
+import scala.collection.JavaConverters._
 
 object CDKMain {
   def main(args: Array[String]): Unit = {
@@ -15,7 +21,38 @@ object CDKMain {
 }
 
 class WebAppStack(scope: Construct, name: String) extends Stack(scope, name) {
-  val vpc = new Vpc(this, "Vpc", VpcProps.builder().maxAzs(1).build())
+  val vpc: Vpc = new Vpc(this, "Vpc", VpcProps.builder().maxAzs(2).build())
 
-  val cluster = new Cluster(this, "Cluster", ClusterProps.builder().vpc(vpc).build())
+  val cluster: Cluster = new Cluster(this, "Cluster", ClusterProps.builder().vpc(vpc).build())
+
+  val taskDefinition: FargateTaskDefinition = {
+    val props   = FargateTaskDefinitionProps.builder().cpu(256).memoryLimitMiB(1024).build()
+    val taskDef = new FargateTaskDefinition(this, "TaskDefinition", props)
+
+    val container = {
+      val opts = ContainerDefinitionOptions
+        .builder()
+        .image(ContainerImage.fromAsset("./server/target/docker/stage"))
+        .environment(Map("APPLICATION_SECRET" -> sys.env("APPLICATION_SECRET")).asJava)
+        .logging(LogDriver.awsLogs(AwsLogDriverProps.builder().streamPrefix("webserver").build()))
+        .build()
+      taskDef.addContainer("WebServerContainer", opts)
+    }
+    container.addPortMappings(PortMapping.builder().containerPort(9000).build())
+
+    taskDef
+  }
+
+  val service: ApplicationLoadBalancedFargateService = {
+    val props = ApplicationLoadBalancedFargateServiceProps
+      .builder()
+      .cluster(cluster)
+      .taskDefinition(taskDefinition)
+      .publicLoadBalancer(true)
+      .desiredCount(1)
+      .enableEcsManagedTags(true)
+      .propagateTags(PropagatedTagSource.SERVICE)
+      .build()
+    new ApplicationLoadBalancedFargateService(this, "Service", props)
+  }
 }
